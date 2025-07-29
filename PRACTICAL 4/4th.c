357ed1259aad8e5ac2381a8f8b72ad5c4b80d5db
Veera Patel <veerapatel.ce@gmail.com>
2025-07-29 13:36:59 +0530

    Add files via upload

---
+++ PRACTICAL 4/4th.c
@@ -0,0 +1,13 @@
+//to show datatypes and their ranges
+#include <stdio.h>
+void main()
+{
+    int a;
+    char b;
+    float c;
+    double d;
+    printf("The size of integer is: %d" ,sizeof(int));
+    printf("\9nThe size of character is: %d", sizeof(char));
+    printf("\nThe size of float is: %d" ,sizeof(float));
+    printf("\nThe size of double is: %d" ,sizeof(double));
+}
