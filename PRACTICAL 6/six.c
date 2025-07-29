c76d982c08123657be25aa21315800be9512046f
Veera Patel <veerapatel.ce@gmail.com>
2025-07-29 10:08:24 +0530

    Delete PRACTICAL 6 directory

--- PRACTICAL 6/six.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main()
-{
-    float temp_cel,temp_fah;
-    printf("Enter the temperature in Celsius:");
-    scanf("%f",&temp_cel);
-    temp_fah=((temp_cel*9)/5)+32;
-    printf("\nThe temperature in Fahrenheit is: %f ",temp_fah);
-}
